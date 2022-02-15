d053c342d33bd42bca7b2ba1e24543a8db07e647
Vitor Natario <vitor.natario@opah.com.br>
2022-02-15 09:39:51 -0300

    Card Props

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,7 +40,7 @@ export function Home(){
 
             {
                 mySkills.map(skill => (
-                   <SkillCard/>
+                   <SkillCard skill={skill}/>
                 ))
             }
         </View>
